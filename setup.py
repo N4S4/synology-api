97f082b36947047e21c029de0e510524a9bac83b
Renato Visaggio <33936751+N4S4@users.noreply.github.com>
2019-09-06 13:16:53 +0200

    Added virtalization (#6)
    * added virtualization
    
    * virtualization added
    
    I can't test the new virtualization file as my station does not support it.
    I would be glad if someone could do it for me and give me some feedback.
    
    thank you
    
    * Update __init__.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synology-api',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Python Synology API Wrapper',
