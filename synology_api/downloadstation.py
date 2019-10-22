8ad2915feefb6c98d51d444ff00e2192df573999
Andrew Mack <andrewmack@benzinga.com>
2019-10-22 14:33:24 -0400

    continued simplification of code

--- synology_api/downloadstation.py
+++ synology_api/downloadstation.py
@@ -2,25 +2,28 @@ from .synology import Synology
 
 class DownloadStation(Synology):
 
+    #this is a function so that ithe value returned is not accidentally changed
+    def app(self):
+        return 'DownloadStation'
+
     def __init__(self, ip_address, port, username, password):
         super(DownloadStation, self).__init__(ip_address, port, username,
                 password)
-        self.app = 'DownloadStation'
         self._bt_search_id = ''
         self._bt_search_id_list = []
-        self.login(self.app)
-        self.populate_api_dict(self.app)
+        self.login(self.app())
+        self.populate_api_dict(self.app())
 
     def logout(self):
-        self.logout(self.app)
+        super().logout(self.app())
     
     @self.api_call
     def get_info(self):
-        return self.api_request(self.app, 'Info', 'getinfo')
+        return self.api_request(self.app(), 'Info', 'getinfo')
     
     @self.api_call
     def get_config(self):
-        return self.api_request(self.app, 'Info', 'getconfig')
+        return self.api_request(self.app(), 'Info', 'getconfig')
     
     """
     Method: set_server_config
@@ -39,12 +42,12 @@ class DownloadStation(Synology):
         param = {}
         for key in kwargs.keys():
             param[str(key)] = kwargs[key]
-        return self.api_request(self.app, 'Info', 'setserverconfig',
+        return self.api_request(self.app(), 'Info', 'setserverconfig',
                 param=param)
     
     @self.api_call
     def schedule_info(self):
-        return self.api_request(self.app, 'Schedule', 'getconfig')
+        return self.api_request(self.app(), 'Schedule', 'getconfig')
     
     @self.api_call
     def schedule_set_config(self, enabled=False, emule_enabled=False):
@@ -55,7 +58,7 @@ class DownloadStation(Synology):
 
         param = {'enabled': str(enabled).lower(), 
                  'emule_enabled': str(emule_enabled).lower()}
-        return self.api_request(self.app, 'Schedule', 'setconfig', param)
+        return self.api_request(self.app(), 'Schedule', 'setconfig', param)
     
     @self.api_call
     def tasks_list(self, additional_param=None):
@@ -64,7 +67,7 @@ class DownloadStation(Synology):
 
         param = {'additional': ",".join(additional_param)}
 
-        return self.api_request(self.app, 'Task', 'list', param)
+        return self.api_request(self.app(), 'Task', 'list', param)
     
     @self.api_call
     def tasks_info(self, task_id, additional_param=None):
@@ -73,7 +76,7 @@ class DownloadStation(Synology):
 
         param = {'additional': ",".join(additional_param)}
 
-        return self.api_request(self.app, 'Task', 'getinfo', param)
+        return self.api_request(self.app(), 'Task', 'getinfo', param)
     
     @self.api_call
     def delete_task(self, task_id, force=False):
@@ -81,7 +84,7 @@ class DownloadStation(Synology):
             task_id = ",".join(task_id) #task_id is now type str
         param = {'id': task_id, 'force_complete': str(force).lower()}
 
-        return self.api_request(self.app, 'Task', 'delete', param)
+        return self.api_request(self.app(), 'Task', 'delete', param)
     
     @self.api_call
     def pause_task(self, task_id):
@@ -89,7 +92,7 @@ class DownloadStation(Synology):
             task_id  = ",".join(task_id) #task_id is now type str
         param = {'id': task_id}
 
-        return self.api_request(self.app, 'Task', 'pause', param)
+        return self.api_request(self.app(), 'Task', 'pause', param)
 
     @self.api_call
     def resume_task(self, task_id): 
@@ -97,7 +100,7 @@ class DownloadStation(Synology):
             task_id = ",".join(task_id) #task_id is now type str
         param = {'id': task_id}
 
-        return self.api_request(self.app, 'Task', 'resume', param)
+        return self.api_request(self.app(), 'Task', 'resume', param)
     
     @self.api_call
     def edit_task(self, task_id, destination='sharedfolder'):
@@ -105,51 +108,41 @@ class DownloadStation(Synology):
             task_id = ",".join(task_id) #task_id is now type str
         param = {'id': task_id, 'destination': destination}
 
-        return self.api_request(self.app, 'Task', 'edit', param)
+        return self.api_request(self.app(), 'Task', 'edit', param)
     
     @self.api_call
     def get_statistic_info(self):
-        return self.api_request(self.app, 'Statistic', 'getinfo')
+        return self.api_request(self.app(), 'Statistic', 'getinfo')
     
     @self.api_call
     def get_rss_info_list(self, offset=None, limit=None):
-        api_name = 'SYNO.DownloadStation.RSS.Site'
-        info = self.download_list[api_name]
-        api_path = info['path']
-        param = {'version': info['maxVersion'], 'method': 'list'}
-
+        
+        param = {}
         if offset is not None:
             param['offset'] = offset
         if limit is not None:
             param['limit'] = limit
+        if len(param) == 0:
+            param = None
 
-        return self.request_data(api_name, api_path, param)
+        return self.api_request(self.app(), 'RSS.Site', 'list',  param)
     
     @self.api_call
-    def refresh_rss_site(self, rss_id=None):
-        api_name = 'SYNO.DownloadStation.RSS.Site'
-        info = self.download_list[api_name]
-        api_path = info['path']
-        param = {'version': info['maxVersion'], 'method': 'refresh', 'id': rss_id}
+    def refresh_rss_site(self, rss_id):
 
-        if rss_id is None:
-            return 'Enter a valid ID check if you have any with get_rss_list()'
-        elif type(rss_id) is list:
+        param = {'id': rss_id}
+        if type(rss_id) is list:
             rss_id = ','.join(rss_id)
             param['id'] = rss_id
 
-        return self.request_data(api_name, api_path, param)
+        return self.api_request(self.app(), 'RSS.Site', 'refresh', param)
     
     @self.api_call
-    def rss_feed_list(self, rss_id=None, offset=None, limit=None):
-        api_name = 'SYNO.DownloadStation.RSS.Feed'
-        info = self.download_list[api_name]
-        api_path = info['path']
-        param = {'version': info['maxVersion'], 'method': 'list', 'id': rss_id}
+    def rss_feed_list(self, offset=None, limit=None):
+        
+        param = {'id': rss_id}
 
-        if rss_id is None:
-            return 'Enter a valid ID check if you have any with get_rss_list()'
-        elif type(rss_id) is list:
+        if type(rss_id) is list:
             rss_id = ','.join(rss_id)
             param['id'] = rss_id
 
@@ -158,81 +151,55 @@ class DownloadStation(Synology):
         if limit is not None:
             param['limit'] = limit
 
-        return self.request_data(api_name, api_path, param)
+        return self.api_request(self.app(), 'RSS.Feed', 'list', param)
 
     @self.api_call
-    def start_bt_search(self, keyword=None, module='all'):
-        api_name = 'SYNO.DownloadStation.BTSearch'
-        info = self.download_list[api_name]
-        api_path = info['path']
-        param = {'version': info['maxVersion'], 'method': 'start'}
+    def start_bt_search(self, keyword, module='all'):
 
-        if keyword is None:
-            return 'Did you enter a keyword to search?'
-        else:
-            param['keyword'] = keyword
+        param = {'keyword': keyword, 'module': module}
 
-        param['module'] = module
-
-        self._bt_search_id = self.request_data(api_name, api_path, param)['data']['taskid']
+        self._bt_search_id = self.api_request(self.app(), 'BTSearch', 'start', param)['data']['taskid']
 
         self._bt_search_id_list.append(self._bt_search_id)
 
-        return 'You can now check the status of request with get_bt_search_results(), your id is: ' + self._bt_search_id
-
+        return self._bt_search_id
+    
+    """
+    method: get_bt_search_results
+    args: taskid,
+          offset,
+          limit,
+          sort_by,
+          sort_direction,
+          filter_category,
+          filter_title
+    """
     @self.api_call
-    def get_bt_search_results(self, taskid=None, offset=None, limit=None, sort_by=None, sort_direction=None,
-                              filter_category=None, filter_title=None):
-        api_name = 'SYNO.DownloadStation.BTSearch'
-        info = self.download_list[api_name]
-        api_path = info['path']
-        param = {'version': info['maxVersion'], 'method': 'list', 'taskid': taskid}
-
-        for key, val in locals().items():
-            if key not in ['self', 'api_name', 'info', 'api_path', 'param', 'taskid']:
-                if val is not None:
-                    param[str(key)] = val
-
-        if taskid is None:
-            return 'Enter a valid taskid, you can choose one of ' + str(self._bt_search_id_list)
-        elif type(taskid) is list:
+    def get_bt_search_results(self, taskid, **kwargs):
+        param = {}
+        if type(taskid) is list:
             param['taskid'] = ','.join(taskid)
+        param.update(kwargs)
 
-        return self.request_data(api_name, api_path, param)
+        return self.api_request(self.app(), 'BTSearch', 'list', param)
     
     @self.api_call
     def get_bt_search_category(self):
-        api_name = 'SYNO.DownloadStation.BTSearch'
-        info = self.download_list[api_name]
-        api_path = info['path']
-        param = {'version': info['maxVersion'], 'method': 'get'}
-
-        return self.request_data(api_name, api_path, param)
+        return self.api_request(self.app(), 'BTSearch', 'get')
 
     @self.api_call
-    def clean_bt_search(self, taskid=None):
-        api_name = 'SYNO.DownloadStation.BTSearch'
-        info = self.download_list[api_name]
-        api_path = info['path']
-        param = {'version': info['maxVersion'], 'method': 'clean', 'taskid': taskid}
+    def clean_bt_search(self, taskid):
 
-        if taskid is None:
-            return 'Enter a valid taskid, you can choose one of ' + str(self._bt_search_id_list)
-        elif type(taskid) is list:
+        param = {'taskid': taskid}
+        if type(taskid) is list:
             param['taskid'] = ','.join(taskid)
             for item in taskid:
                 self._bt_search_id_list.remove(item)
         else:
             self._bt_search_id_list.remove(taskid)
 
-        return self.request_data(api_name, api_path, param)
+        return self.api_request(self.app(), 'BTSearch', 'path', param)
 
     @self.api_call
     def get_bt_module(self):
-        api_name = 'SYNO.DownloadStation.BTSearch'
-        info = self.download_list[api_name]
-        api_path = info['path']
-        param = {'version': info['maxVersion'], 'method': 'getModule'}
-
-        return self.request_data(api_name, api_path, param)
-
+        return self.api_request(self.app(), 'BTSearch', 'getModule'))
