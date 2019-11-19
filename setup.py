80553484717ed709de934f6409e60925c66449d4
emelbaer <39346539+emelbaer@users.noreply.github.com>
2019-11-19 22:32:17 +0100

    Update setup.py
    major version changed cased by function name changed

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='synology-api',
-    version='0.0.3',
+    version='0.1.0',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Python Synology API Wrapper',
     long_description=open('README.md').read(),
     install_requires=['requests'],
-    url='https://github.com/N4S4/synology-api',
-    author='Renato Visaggio',
-    author_email='renatovisaggio@gmail.com'
+    url='https://github.com/emelbaer/synology-api',
+    author='Emel B (c) Renato Visaggio',
+    author_email=''
 )
