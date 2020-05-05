f9f11a04c28e8e4bcc40e6a4cd745d457f06c744
therealj4nd3rs0n <64821959+therealj4nd3rs0n@users.noreply.github.com>
2020-05-04 23:11:22 -0500

    Create backup.py

---
+++ synology_api/backup.py
@@ -0,0 +1,63 @@
+from . import auth as syn
+
+
+class SysInfo:
+    def __init__(self, ip_address, port, username, password):
+
+        self.session = syn.Authentication(ip_address, port, username, password)
+
+        self.session.login('Core')
+        self.session.get_api_list('Core')
+        self.session.get_api_list()
+
+        self.request_data = self.session.request_data
+        self.core_list = self.session.app_api_list
+        self.gen_list = self.session.full_api_list
+        self._sid = self.session.sid
+        self.base_url = self.session.base_url
+
+        print('You are now logged in!')
+
+    def logout(self):
+        self.session.logout('Core')
+
+    def backup_repository_get(self):
+        api_name = 'SYNO.Backup.Repository'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'get'}
+
+        return self.request_data(api_name, api_path, req_param)
+        
+    def backup_repository_list(self):
+        api_name = 'SYNO.Backup.Repository'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'list'}
+
+        return self.request_data(api_name, api_path, req_param)
+        
+    def backup_task_list(self):
+        api_name = 'SYNO.Backup.Task'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'list'}
+
+        return self.request_data(api_name, api_path, req_param)
+        
+    def backup_task_status(self):
+        api_name = 'SYNO.Backup.Task'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'status'}
+
+        return self.request_data(api_name, api_path, req_param)
+        
+     def backup_task_get(self):
+        api_name = 'SYNO.Backup.Task'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'get'}
+
+        return self.request_data(api_name, api_path, req_param)
+
