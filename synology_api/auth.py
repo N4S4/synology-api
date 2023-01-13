fa150591e9dd90e31095d6e2eec736128f1bb330
pnearing <me@peternearing.ca>
2023-01-13 09:28:34 -0500

    Type auth.py

--- synology_api/auth.py
+++ synology_api/auth.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import requests
 from .error_codes import error_codes, CODE_SUCCESS, CODE_UNKNOWN
 from urllib3 import disable_warnings
@@ -5,30 +6,40 @@ from urllib3.exceptions import InsecureRequestWarning
 
 
 class Authentication:
-    def __init__(self, ip_address, port, username, password, secure=False, cert_verify=False, dsm_version=7, debug=True,
-                 otp_code=None):
-        self._ip_address = ip_address
-        self._port = port
-        self._username = username
-        self._password = password
-        self._sid = None
-        self._session_expire = True
-        self._verify = cert_verify
-        self._version = dsm_version
-        self._debug = debug
-        self._otp_code = otp_code
+    def __init__(self,
+                    ip_address : str,
+                    port : str,
+                    username : str,
+                    password : str,
+                    secure : bool = False,
+                    cert_verify : bool = False,
+                    dsm_version : int = 7,
+                    debug : bool = True,
+                    otp_code : Optional[str] = None
+                ) -> None:
+        self._ip_address : str = ip_address
+        self._port : str = port
+        self._username : str = username
+        self._password : str = password
+        self._sid : Optional[str] = None
+        self._session_expire : bool = True
+        self._verify : bool = cert_verify
+        self._version : int = dsm_version
+        self._debug : bool = debug
+        self._otp_code : Optional[str] = otp_code
         if self._verify is False:
             disable_warnings(InsecureRequestWarning)
-        schema = 'https' if secure else 'http'
-        self._base_url = '%s://%s:%s/webapi/' % (schema, self._ip_address, self._port)
+        schema : str = 'https' if secure else 'http'
+        self._base_url : str = '%s://%s:%s/webapi/' % (schema, self._ip_address, self._port)
 
-        self.full_api_list = {}
-        self.app_api_list = {}
+        self.full_api_list : dict[str, object] = {}
+        self.app_api_list : dict[str, object] = {}
+        return
 
-    def verify_cert_enabled(self):
+    def verify_cert_enabled(self) -> bool:
         return self._verify
 
-    def login(self, application):
+    def login(self, application:str) -> None:
         login_api = 'auth.cgi?api=SYNO.API.Auth'
         params = {'version': self._version, 'method': 'login', 'account': self._username,
                   'passwd': self._password, 'session': application, 'format': 'cookie'}
@@ -52,8 +63,9 @@ class Authentication:
                 self._sid = None
                 if self._debug is True:
                     print('Login failed: ' + self._get_error_message(error_code))
+        return
 
-    def logout(self, application):
+    def logout(self, application:str) -> None:
         logout_api = 'auth.cgi?api=SYNO.API.Auth'
         param = {'version': self._version, 'method': 'logout', 'session': application}
 
@@ -66,8 +78,9 @@ class Authentication:
                 print('Successfully logged out.')
             else:
                 print('Logout failed: ' + self._get_error_message(error_code))
+        return
 
-    def get_api_list(self, app=None):
+    def get_api_list(self, app:str=None) -> None:
         query_path = 'query.cgi?api=SYNO.API.Info'
         list_query = {'version': '1', 'method': 'query', 'query': 'all'}
 
@@ -82,7 +95,7 @@ class Authentication:
 
         return
 
-    def show_api_name_list(self):
+    def show_api_name_list(self) -> None:
         prev_key = ''
         for key in self.full_api_list:
             if key != prev_key:
@@ -90,7 +103,7 @@ class Authentication:
                 prev_key = key
         return
 
-    def show_json_response_type(self):
+    def show_json_response_type(self) -> None:
         for key in self.full_api_list:
             for sub_key in self.full_api_list[key]:
                 if sub_key == 'requestFormat':
@@ -98,7 +111,7 @@ class Authentication:
                         print(key + '   Returns JSON data')
         return
 
-    def search_by_app(self, app):
+    def search_by_app(self, app:str) -> None:
         print_check = 0
         for key in self.full_api_list:
             if app.lower() in key.lower():
@@ -109,7 +122,13 @@ class Authentication:
             print('Not Found')
         return
 
-    def request_data(self, api_name, api_path, req_param, method=None, response_json=True):  # 'post' or 'get'
+    def request_data(self,
+                        api_name:str,
+                        api_path:str,
+                        req_param:dict,
+                        method:Optional[str]=None,
+                        response_json:bool=True
+                    ) -> dict[str, object] | requests.models.Response:  # 'post' or 'get'
 
         # Convert all boolean in string in lowercase because Synology API is waiting for "true" or "false"
         for k, v in req_param.items():
@@ -140,7 +159,7 @@ class Authentication:
             return response
 
     @staticmethod
-    def _get_error_code(response: dict):
+    def _get_error_code(response: dict) -> int:
         if response.get('success'):
             code = CODE_SUCCESS
         else:
@@ -153,9 +172,9 @@ class Authentication:
         return 'Error {} - {}'.format(code, message)
 
     @property
-    def sid(self):
+    def sid(self) -> str:
         return self._sid
 
     @property
-    def base_url(self):
+    def base_url(self) -> str:
         return self._base_url
