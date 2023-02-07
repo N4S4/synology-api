228cb14fab08861f90fb422198da4db70bf37da8
fortysix2ahead <fortysix2ahead@gmail.com>
2023-02-07 12:20:05 +0100

    Added appdirs as requirement

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
+        'appdirs',
         'click',
         'requests',
         'urllib3',
