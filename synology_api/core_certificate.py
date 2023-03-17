a526163d14094cdeba175de83da58d17c18eec1c
pnearing <me@peternearing.ca>
2023-03-17 15:59:49 -0400

    Clean up indentation. Fix __init__ return type..

--- synology_api/core_certificate.py
+++ synology_api/core_certificate.py
@@ -10,20 +10,26 @@ import json
 
 class Certificate(base_api_core.Core):
     def __init__(self,
-                    ip_address: str,
-                    port: str,
-                    username: str,
-                    password: str,
-                    secure: bool = False,
-                    cert_verify: bool = False,
-                    dsm_version: int = 7,
-                    debug: bool = True,
-                    otp_code: Optional[str] = None):
-        super(Certificate, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version, debug, otp_code)
-        self._debug : bool = debug
+                 ip_address: str,
+                 port: str,
+                 username: str,
+                 password: str,
+                 secure: bool = False,
+                 cert_verify: bool = False,
+                 dsm_version: int = 7,
+                 debug: bool = True,
+                 otp_code: Optional[str] = None
+                 ) -> None:
+        super(Certificate, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version, debug,
+                                          otp_code)
+        self._debug: bool = debug
         return
 
-    def _base_certificate_methods(self, method: str, cert_id: Optional[str]=None, ids:Optional[str|list[str]]=None) -> str | dict[str, object]:
+    def _base_certificate_methods(self,
+                                  method: str,
+                                  cert_id: Optional[str] = None,
+                                  ids: Optional[str | list[str]] = None
+                                  ) -> str | dict[str, object]:
         available_method = ['list', 'set', 'delete']
         if method not in available_method:
             # print error here
@@ -54,18 +60,18 @@ class Certificate(base_api_core.Core):
     def set_default_cert(self, cert_id: str) -> dict[str, object]:
         return self._base_certificate_methods('set', cert_id)
 
-    def delete_certificate(self, ids: str|list[str]) -> dict[str,object]:
+    def delete_certificate(self, ids: str | list[str]) -> dict[str, object]:
         if isinstance(ids, str):
             ids = [ids]
         return self._base_certificate_methods('delete', ids=ids)
 
     def upload_cert(self,
-                        serv_key: str = "server.key",
-                        ser_cert: str = "server.crt",
-                        ca_cert: Optional[str] = None,
-                        set_as_default: bool = True,
-                        cert_id: Optional[str] = None,
-                        desc: Optional[str] = None
+                    serv_key: str = "server.key",
+                    ser_cert: str = "server.crt",
+                    ca_cert: Optional[str] = None,
+                    set_as_default: bool = True,
+                    cert_id: Optional[str] = None,
+                    desc: Optional[str] = None
                     ) -> tuple[int, dict[str, object]]:
         api_name = 'SYNO.Core.Certificate'
         info = self.session.app_api_list[api_name]
@@ -78,7 +84,7 @@ class Certificate(base_api_core.Core):
         session = requests.session()
 
         url = ('%s%s' % (self.base_url, api_path)) + '?api=%s&version=%s&method=import&_sid=%s' % (
-                    api_name, info['minVersion'], self._sid)
+            api_name, info['minVersion'], self._sid)
 
         if cert_id:
             print("update exist cert: " + cert_id)
