e8287cc19cf070c6862cd0d7dbb39f1e0177beea
pnearing <me@peternearing.ca>
2023-03-17 16:24:18 -0400

    Clean up indentation, and add | str to return types

--- synology_api/oauth.py
+++ synology_api/oauth.py
@@ -4,20 +4,21 @@ from . import base_api_core
 
 class OAuth(base_api_core.Core):
     def __init__(self,
-                    ip_address: str,
-                    port: str,
-                    username: str,
-                    password: str,
-                    secure: bool = False,
-                    cert_verify: bool = False,
-                    dsm_version: int = 7,
-                    debug: bool = True,
-                    otp_code: Optional[str] = None
-                ) -> None:
-        super(OAuth, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version, debug, otp_code)
+                 ip_address: str,
+                 port: str,
+                 username: str,
+                 password: str,
+                 secure: bool = False,
+                 cert_verify: bool = False,
+                 dsm_version: int = 7,
+                 debug: bool = True,
+                 otp_code: Optional[str] = None
+                 ) -> None:
+        super(OAuth, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version, debug,
+                                    otp_code)
         return
 
-    def clients(self, offset:int=0, limit:int=20) -> dict[str, object]:
+    def clients(self, offset: int = 0, limit: int = 20) -> dict[str, object] | str:
         api_name = 'SYNO.OAUTH.Client'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -25,7 +26,7 @@ class OAuth(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def tokens(self, offset:int=0, limit:int=20) -> dict[str, object]:
+    def tokens(self, offset: int = 0, limit: int = 20) -> dict[str, object] | str:
         api_name = 'SYNO.OAUTH.Token'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -33,7 +34,7 @@ class OAuth(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def logs(self, offset:int=0, limit:int=20) -> dict[str, object]:
+    def logs(self, offset: int = 0, limit: int = 20) -> dict[str, object] | str:
         api_name = 'SYNO.OAUTH.Log'
         info = self.gen_list[api_name]
         api_path = info['path']
