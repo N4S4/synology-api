62b9f161b6807b213d9a7cb71871af4dbfc7b74e
pnearing <me@peternearing.ca>
2023-03-17 16:06:11 -0400

    Clean up indentation. Fix return types by adding | str.

--- synology_api/dhcp_server.py
+++ synology_api/dhcp_server.py
@@ -4,20 +4,21 @@ from . import base_api_core
 
 class DhcpServer(base_api_core.Core):
     def __init__(self,
-                    ip_address: str,
-                    port: str,
-                    username: str,
-                    password: str,
-                    secure: bool = False,
-                    cert_verify: bool = False,
-                    dsm_version: int = 7,
-                    debug: bool = True,
-                    otp_code:Optional[str] = None
-                ) -> None:
-        super(DhcpServer, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version, debug, otp_code)
+                 ip_address: str,
+                 port: str,
+                 username: str,
+                 password: str,
+                 secure: bool = False,
+                 cert_verify: bool = False,
+                 dsm_version: int = 7,
+                 debug: bool = True,
+                 otp_code: Optional[str] = None
+                 ) -> None:
+        super(DhcpServer, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version, debug,
+                                         otp_code)
         return
-    
-    def general_info(self) -> dict[str, object]:
+
+    def general_info(self) -> dict[str, object] | str:
         api_name = 'SYNO.Network.DHCPServer'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -25,7 +26,7 @@ class DhcpServer(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def vendor(self) -> dict[str, object]:
+    def vendor(self) -> dict[str, object] | str:
         api_name = 'SYNO.Network.DHCPServer.Vendor'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -33,7 +34,7 @@ class DhcpServer(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def pxe(self) -> dict[str, object]:
+    def pxe(self) -> dict[str, object] | str:
         api_name = 'SYNO.Network.DHCPServer.PXE'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -41,7 +42,7 @@ class DhcpServer(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def tftp(self) -> dict[str, object]:
+    def tftp(self) -> dict[str, object] | str:
         api_name = 'SYNO.Core.TFTP'
         info = self.core_list[api_name]
         api_path = info['path']
@@ -49,7 +50,7 @@ class DhcpServer(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def network_bond(self) -> dict[str, object]:
+    def network_bond(self) -> dict[str, object] | str:
         api_name = 'SYNO.Core.Network.Bond'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -57,7 +58,7 @@ class DhcpServer(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def network_ethernet(self) -> dict[str, object]:
+    def network_ethernet(self) -> dict[str, object] | str:
         api_name = 'SYNO.Core.Network.Ethernet'
         info = self.gen_list[api_name]
         api_path = info['path']
