d87c971d7238b6ad5cf6542cabcf7176ff22499e
pnearing <me@peternearing.ca>
2023-03-17 18:16:25 -0400

    Clean up indentation, and add | str to return types

--- synology_api/security_advisor.py
+++ synology_api/security_advisor.py
@@ -4,20 +4,21 @@ from . import base_api_core
 
 class SecurityAdvisor(base_api_core.Core):
     def __init__(self,
-                    ip_address: str,
-                    port: str,
-                    username: str,
-                    password: str,
-                    secure: bool = False,
-                    cert_verify: bool = False,
-                    dsm_version: int = 7,
-                    debug: bool = True,
-                    otp_code: Optional[str] = None
-                ) -> None:
-        super(SecurityAdvisor, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version, debug, otp_code)
+                 ip_address: str,
+                 port: str,
+                 username: str,
+                 password: str,
+                 secure: bool = False,
+                 cert_verify: bool = False,
+                 dsm_version: int = 7,
+                 debug: bool = True,
+                 otp_code: Optional[str] = None
+                 ) -> None:
+        super(SecurityAdvisor, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version,
+                                              debug, otp_code)
         return
 
-    def general_info(self) -> dict[str, object]:
+    def general_info(self) -> dict[str, object] | str:
         api_name = 'SYNO.SecurityAdvisor.Conf.Location'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -25,7 +26,7 @@ class SecurityAdvisor(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def security_scan(self) -> dict[str, object]:
+    def security_scan(self) -> dict[str, object] | str:
         api_name = 'SYNO.Core.SecurityScan.Conf'
         info = self.core_list[api_name]
         api_path = info['path']
@@ -33,7 +34,7 @@ class SecurityAdvisor(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def checklist(self) -> dict[str, object]:
+    def checklist(self) -> dict[str, object] | str:
         api_name = 'SYNO.SecurityAdvisor.Conf.Checklist'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -41,7 +42,7 @@ class SecurityAdvisor(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def login_activity(self, offset:int=0, limit:int=20) -> dict[str, object]:
+    def login_activity(self, offset: int = 0, limit: int = 20) -> dict[str, object] | str:
         api_name = 'SYNO.SecurityAdvisor.LoginActivity'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -49,7 +50,7 @@ class SecurityAdvisor(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def advisor_config(self) -> dict[str, object]:
+    def advisor_config(self) -> dict[str, object] | str:
         api_name = 'SYNO.SecurityAdvisor.Conf'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -57,7 +58,7 @@ class SecurityAdvisor(base_api_core.Core):
 
         return self.request_data(api_name, api_path, req_param)
 
-    def scan_config(self) -> dict[str, object]:
+    def scan_config(self) -> dict[str, object] | str:
         api_name = 'SYNO.Core.SecurityScan.Conf'
         info = self.gen_list[api_name]
         api_path = info['path']
