7fa41415c128a3255ff1c5a873a82c1c4e7ea774
pnearing <me@peternearing.ca>
2023-03-17 18:17:53 -0400

    Clean up indentation, and add | str to return types

--- synology_api/universal_search.py
+++ synology_api/universal_search.py
@@ -4,29 +4,31 @@ from synology_api import auth
 
 class UniversalSearch:
     def __init__(self,
-                    ip_address: str,
-                    port: str,
-                    username: str,
-                    password: str,
-                    secure: bool = False,
-                    cert_verify: bool = False,
-                    dsm_version: int = 7,
-                    debug: bool = True,
-                    otp_code: Optional[str] = None
-                ) -> None:
-        self.session : auth.Authentication = auth.Authentication(ip_address, port, username, password, secure, cert_verify, dsm_version, debug, otp_code)
+                 ip_address: str,
+                 port: str,
+                 username: str,
+                 password: str,
+                 secure: bool = False,
+                 cert_verify: bool = False,
+                 dsm_version: int = 7,
+                 debug: bool = True,
+                 otp_code: Optional[str] = None
+                 ) -> None:
+        self.session: auth.Authentication = auth.Authentication(ip_address, port, username, password, secure,
+                                                                cert_verify, dsm_version, debug, otp_code)
         self.session.login('Finder')
         self.session.get_api_list('Finder')
-        self.request_data : Any = self.session.request_data
-        self.finder_list :Any = self.session.app_api_list
-        self._sid : str = self.session.sid
-        self.base_url : str = self.session.base_url
+        self.request_data: Any = self.session.request_data
+        self.finder_list: Any = self.session.app_api_list
+        self._sid: str = self.session.sid
+        self.base_url: str = self.session.base_url
+        return
 
     def logout(self) -> None:
         self.session.logout('FileStation')
         return
 
-    def search(self, keyword:str) -> dict[str, object]:
+    def search(self, keyword: str) -> dict[str, object] | str:
         api_name = 'SYNO.Finder.FileIndexing.Search'
         info = self.finder_list[api_name]
         api_path = info['path']
