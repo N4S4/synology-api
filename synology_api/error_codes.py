a2d35acdebf4353508634a1a48ba74d262efa68f
Renato <33936751+N4S4@users.noreply.github.com>
2023-03-20 21:04:44 +0100

    cleanup code

--- synology_api/error_codes.py
+++ synology_api/error_codes.py
@@ -1,8 +1,8 @@
 # source: pages 8 and 16 on PDF:
 # https://global.download.synology.com/download/Document/Software/DeveloperGuide/Os/DSM/All/enu/DSM_Login_Web_API_Guide_enu.pdf
 
-# NOTE: https://global.synologydownload.com/download/Document/Software/DeveloperGuide/Package/Calendar/2.4/enu/Synology_Calendar_API_Guide_enu.pdf,
-# Refers to common error code # 160, "Insufficient application privilege" Page 10.
+# NOTE: https://global.synologydownload.com/download/Document/Software/DeveloperGuide/Package/Calendar/2.4/enu
+# /Synology_Calendar_API_Guide_enu.pdf, Refers to common error code # 160, "Insufficient application privilege" Page 10.
 
 CODE_SUCCESS = 0
 CODE_UNKNOWN = 9999
@@ -64,11 +64,12 @@ error_codes = {
     160: 'Insufficient application privilege',
     CODE_UNKNOWN: 'Unknown Error',
 }
-# Source: https://global.synologydownload.com/download/Document/Software/DeveloperGuide/Os/DSM/All/enu/DSM_Login_Web_API_Guide_enu.pdf
-# Page 16.
-# https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package/SurveillanceStation/All/enu/Surveillance_Station_Web_API.pdf
-# Pages 32,33 Refers to Auth error code #411: 'Account Locked (when account max try exceed).'
-auth_error_codes : dict[int, str] = {
+# Source: https://global.synologydownload.com/download/Document/Software/DeveloperGuide/Os/DSM/All/enu
+# /DSM_Login_Web_API_Guide_enu.pdf Page 16.
+# https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package/SurveillanceStation/All/enu
+# /Surveillance_Station_Web_API.pdf Pages 32,33 Refers to Auth error code #411: 'Account Locked (when account max try
+# exceed).'
+auth_error_codes: dict[int, str] = {
     400: 'No such account or incorrect password',
     401: 'Disabled account',
     402: 'Denied permission',
@@ -84,7 +85,7 @@ auth_error_codes : dict[int, str] = {
 
 # Source:https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package/DownloadStation/All/enu/Synology_Download_Station_Web_API.pdf
 # Page 29
-download_station_error_codes : dict[int, str] = {
+download_station_error_codes: dict[int, str] = {
     400: 'File upload failed',
     401: 'Max number of tasks reached',
     402: 'Destination denied',
@@ -96,10 +97,9 @@ download_station_error_codes : dict[int, str] = {
     408: 'File does not exist',
 }
 
-# TODO use the error code
-# source https://cndl.synology.cn/download/Document/Software/DeveloperGuide/Package/FileStation/All/enu/Synology_File_Station_API_Guide.pdf
-# page 10~11
-file_station_error_codes : dict[int, str] = {
+# TODO use the error code source https://cndl.synology.cn/download/Document/Software/DeveloperGuide/Package
+#  /FileStation/All/enu/Synology_File_Station_API_Guide.pdf page 10~11
+file_station_error_codes: dict[int, str] = {
     400: 'Invalid parameter of file operation',
     401: 'Unknown error of file operation',
     402: 'System is too busy',
@@ -125,13 +125,14 @@ file_station_error_codes : dict[int, str] = {
     599: 'No such task of the file operation',
 }
 
-# Source: https://global.synologydownload.com/download/Document/Software/DeveloperGuide/Package/Virtualization/All/enu/Synology_Virtual_Machine_Manager_API_Guide.pdf
-# Page 8,9
+# Source: https://global.synologydownload.com/download/Document/Software/DeveloperGuide/Package/Virtualization/All
+# /enu/Synology_Virtual_Machine_Manager_API_Guide.pdf Page 8,9
 virtualization_error_codes: dict[int, str] = {
     401: 'Bad parameter.',
     402: 'Operation failed.',
     403: 'Name conflict.',
-    404: 'The number of iSCSI LUNs has reached the system limit. Note: vdisk is based on iSCSI LUN, which is also limited by the system.',
+    404: 'The number of iSCSI LUNs has reached the system limit. Note: vdisk is based on iSCSI LUN, which is also '
+         'limited by the system.',
     500: 'The cluster is frozen. More than half of the hosts are offline.',
     501: 'The cluster is in the incompatible mode. Please upgrade to a compatible DSM version and try again.',
     600: 'The cluster is not ready.',
@@ -152,12 +153,13 @@ virtualization_error_codes: dict[int, str] = {
     1000: 'Cannot find task_id.',
     1001: 'Need Virtual Machine Manager Pro.',
     1400: 'The result of image creating is partial success.',
-    1600: 'The virtual machine has been successfully edited. However, errors occurred while reserving the memory or CPU on the HA hosts.',
+    1600: 'The virtual machine has been successfully edited. However, errors occurred while reserving the memory or '
+          'CPU on the HA hosts.',
 }
 
-# Source: https://global.synologydownload.com/download/Document/Software/DeveloperGuide/Package/Calendar/2.4/enu/Synology_Calendar_API_Guide_enu.pdf
-# Pages 10,11.
-calendar_error_codes : dict[int, str] = {
+# Source: https://global.synologydownload.com/download/Document/Software/DeveloperGuide/Package/Calendar/2.4/enu
+# /Synology_Calendar_API_Guide_enu.pdf Pages 10,11.
+calendar_error_codes: dict[int, str] = {
     400: 'Invalid parameter of file operation',
     401: 'Unknown error of file operation',
     402: 'System is too busy',
@@ -183,18 +185,18 @@ calendar_error_codes : dict[int, str] = {
     599: 'No such task of the file operation',
 }
 
-# # Source: https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package/SurveillanceStation/All/enu/Surveillance_Station_Web_API.pdf
-# NOTE: Pages 81, 131,132, 155, 190, 222, 305, 314, 321, 328, 451, 473 539, 556, contain unique items.
-# Most of these are duplicate information, but they describe different methods, all the error codes I found are on
-# # Pages: 71, 81, 85, 93, 103, 113, 131, 132, 139, 144, 155, 167, 169, 176, 187, 190, 191, 201, 211, 212, 217, 222, 227,
-# #         241, 245, 249, 253, 264, 279, 281, 305, 314, 321, 328, 363, 365, 368, 369, 393, 395, 397, 403, 410, 412, 415,
-# #         419, 430, 451, 473, 539, 556
-surveillance_station_error_codes : dict[int, str] = {
+# # Source: https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package
+# /SurveillanceStation/All/enu/Surveillance_Station_Web_API.pdf NOTE: Pages 81, 131,132, 155, 190, 222, 305, 314,
+# 321, 328, 451, 473 539, 556, contain unique items. Most of these are duplicate information, but they describe
+# different methods, all the error codes I found are on # Pages: 71, 81, 85, 93, 103, 113, 131, 132, 139, 144, 155,
+# 167, 169, 176, 187, 190, 191, 201, 211, 212, 217, 222, 227, #         241, 245, 249, 253, 264, 279, 281, 305, 314,
+# 321, 328, 363, 365, 368, 369, 393, 395, 397, 403, 410, 412, 415, #         419, 430, 451, 473, 539, 556
+surveillance_station_error_codes: dict[int, str] = {
     400: 'Execution failed.',
     401: 'Parameter invalid.',
     402: 'Camera disabled.',
     403: 'Insufficient license.',
-    404: 'Codec acitvation failed',
+    404: 'Codec activation failed',
     405: 'CMS server connection failed.',
     407: 'CMS closed.',
     412: 'Need to add license.',
@@ -221,5 +223,5 @@ surveillance_station_error_codes : dict[int, str] = {
     562: 'The number of face group exceeds the maximum limit.',
     563: 'The face profile is created or edited failed due to duplicated account.',
     564: 'The face result try to query doesn\'t exist',
-    567: 'The face database is under syncrhonization.',
+    567: 'The face database is under synchronization.',
 }
