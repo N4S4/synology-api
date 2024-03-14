2dfc4420a8ecdacc770a1efbb8905ace10f18d0d
Fabio Testa <fabio.testa@wolterskluwer.com>
2024-03-14 11:26:47 +0100

    reformat file add main to run test suite from commandline

--- tests/test_syno_api.py
+++ tests/test_syno_api.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 from unittest import TestCase
-
+import unittest
 from synology_api.filestation import FileStation
 from synology_api.surveillancestation import SurveillanceStation
 
@@ -20,11 +20,11 @@ class TestSynoApi(TestCase):
 
     def test_syno_filestation_login(self):
         fs = FileStation(ip_address=self.config["synology_ip"], port=self.config["synology_port"],
-                                      username=self.config["synology_user"],
-                                      password=self.config["synology_password"],
-                                      secure=bool(self.config["synology_secure"]), cert_verify=False,
-                                      dsm_version=int(self.config["dsm_version"]), debug=True,
-                                      otp_code=self.config["otp_code"])
+                         username=self.config["synology_user"],
+                         password=self.config["synology_password"],
+                         secure=bool(self.config["synology_secure"]), cert_verify=False,
+                         dsm_version=int(self.config["dsm_version"]), debug=True,
+                         otp_code=self.config["otp_code"])
 
         self.assertIsNotNone(fs)
         self.assertIsNotNone(fs.session)
@@ -36,11 +36,11 @@ class TestSynoApi(TestCase):
 
     def test_syno_surveillancestation_login(self):
         ss = SurveillanceStation(ip_address=self.config["synology_ip"], port=self.config["synology_port"],
-                                      username=self.config["synology_user"],
-                                      password=self.config["synology_password"],
-                                      secure=bool(self.config["synology_secure"]), cert_verify=False,
-                                      dsm_version=int(self.config["dsm_version"]), debug=True,
-                                      otp_code=self.config["otp_code"])
+                                 username=self.config["synology_user"],
+                                 password=self.config["synology_password"],
+                                 secure=bool(self.config["synology_secure"]), cert_verify=False,
+                                 dsm_version=int(self.config["dsm_version"]), debug=True,
+                                 otp_code=self.config["otp_code"])
 
         self.assertIsNotNone(ss)
         self.assertIsNotNone(ss.session)
@@ -51,3 +51,7 @@ class TestSynoApi(TestCase):
         ss_info_data = ss_info['data']
         self.assertIsNotNone(ss_info_data)
         self.assertEqual(ss_info_data['path'], '/webman/3rdparty/SurveillanceStation/')
+
+
+if __name__ == '__main__':
+    unittest.main()
