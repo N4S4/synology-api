ff1c22cd159084b3fdfaae49287b4340d114d4d2
joeperpetua <qwerty.perpetua@gmail.com>
2024-08-16 19:16:59 +0200

    Add docstring

--- synology_api/core_backup.py
+++ synology_api/core_backup.py
@@ -55,6 +55,10 @@ class Backup(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def backup_task_run(self, task_id: str) -> dict[str, object] | str:
+        '''
+        Run backup task for corresponding task_id.
+        If the task is not in backupable state, the API will return an error, usually 44xx.
+        '''
         api_name = 'SYNO.Backup.Task'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -66,6 +70,10 @@ class Backup(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
     
     def backup_task_cancel(self, task_id: str) -> dict[str, object] | str:
+        '''
+        Cancel currently running backup task.
+        If the task is not running, the API will return an error, usually 44xx.
+        '''
         api_name = 'SYNO.Backup.Task'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -78,6 +86,10 @@ class Backup(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
     
     def backup_task_suspend(self, task_id: str) -> dict[str, object] | str:
+        '''
+        Suspend currently running backup task.
+        If the task is not running or not yet suspendable, the API will return an error, usually 44xx.
+        '''
         api_name = 'SYNO.Backup.Task'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -90,6 +102,10 @@ class Backup(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
     
     def backup_task_discard(self, task_id: str) -> dict[str, object] | str:
+        '''
+        Discard currently suspended backup task.
+        If the task is not suspended, the request will not fail, and will fail to discard the task, leaving the task state as "Failed".
+        '''
         api_name = 'SYNO.Backup.Task'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -101,6 +117,10 @@ class Backup(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
     
     def backup_task_resume(self, task_id: str) -> dict[str, object] | str:
+        '''
+        Discard currently suspended backup task.
+        If the task is not suspended, the request will not fail, and will fail to resume the task, leaving the task state as "Failed".
+        '''
         api_name = 'SYNO.Backup.Task'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -112,18 +132,28 @@ class Backup(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
     
     def backup_task_remove(self, task_id_list: str) -> dict[str, object] | str:
+        '''
+        Remove one or more backup tasks.
+        Data in destination will not be removed. It is still possible to relink the task using the original .hbk file.
+        The API requires an array of tasks to remove, it should be passed as a string with the following format:
+        `task_id_list = '[29]'` || `task_id_list = '[29,15]'` 
+        '''
         api_name = 'SYNO.Backup.Task'
         info = self.gen_list[api_name]
         api_path = info['path']
         req_param = {
             'version': info['minVersion'],
             'method': 'delete',
-            'is_remove_data': 'false',
+            'is_remove_data': False,
             'task_id_list': task_id_list
         }
         return self.request_data(api_name, api_path, req_param)
     
     def integrity_check_run(self, task_id: str) -> dict[str, object] | str:
+        '''
+        Run integrity check for backup task.
+        If the task is running, the request will not fail, and will fail to perform the integrity check due to target is busy.
+        '''
         api_name = 'SYNO.Backup.Target'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -138,6 +168,10 @@ class Backup(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
     
     def integrity_check_cancel(self, task_id: str) -> dict[str, object] | str:
+        '''
+        Cancel currently running integrity check for backup task.
+        If integrity check is not running, the API will return an error, usually 44xx.
+        '''
         api_name = 'SYNO.Backup.Target'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -155,6 +189,11 @@ class Backup(base_api.BaseApi):
                     # filter_level: str = '', For some reason when passing filter_level, the API returns error 120.
                     filter_date_from: int = 0,
                     filter_date_to: int = 0) -> dict[str, object] | str:
+        '''
+        Get Hyper Backup UI logs.
+
+        `filter_date_from` and `filter_date_to` need to be passed in epoch format.
+        '''
         api_name = 'SYNO.SDS.Backup.Client.Common.Log'
         info = self.gen_list[api_name]
         api_path = info['path']
