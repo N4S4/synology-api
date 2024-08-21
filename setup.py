ed4fd6aac53da5ff2c24f4cf57c6d334a34eb136
Renato <33936751+N4S4@users.noreply.github.com>
2024-08-21 11:20:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Python Synology API Wrapper',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=['requests', 'urllib3', 'setuptools'],
+    install_requires=['requests', 'urllib3', 'setuptools', 'requests_toolbelt', 'tqdm'],
     url='https://github.com/N4S4/synology-api',
     author='Renato Visaggio',
     author_email='synology.python.api@gmail.com'
