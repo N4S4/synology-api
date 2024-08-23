3a458deeb047cfe9c69a0de64e830d6b745a468a
joeperpetua <qwerty.perpetua@gmail.com>
2024-08-23 11:08:26 +0200

    Adding getters for tasks

--- synology_api/cloud_sync.py
+++ synology_api/cloud_sync.py
@@ -100,3 +100,62 @@ class CloudSync(base_api.BaseApi):
 
         return self.request_data(api_name, api_path, req_param)
 
+    def get_tasks(self, conn_id: int) -> dict[str, object] | str:
+        '''
+        Return list of tasks related to given cloud connection.
+
+        conn_id = int from get_connections
+        '''
+        api_name = 'SYNO.CloudSync'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {
+            'version': info['minVersion'], 
+            'method': 'list_sess',
+            'connection_id': conn_id
+        }
+
+        return self.request_data(api_name, api_path, req_param)
+    
+    def get_task_filters(self, sess_id: int) -> dict[str, object] | str:
+        '''
+        Return filter information for given task.
+
+        sess_id = int from get_tasks
+        '''
+        api_name = 'SYNO.CloudSync'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {
+            'version': info['minVersion'], 
+            'method': 'get_selective_sync_config',
+            'session_id': sess_id
+        }
+
+        return self.request_data(api_name, api_path, req_param)
+
+    def get_task_cloud_folders(
+            self, 
+            sess_id: int,
+            remote_folder_id: str,
+            path: str = '/'
+        ) -> dict[str, object] | str:
+        '''
+        Return list of children directories in Cloud for given task.
+
+        remote_folder_id = str from get_tasks
+        path = str (the folder from which we want the children, default is the task root path)
+        '''
+        api_name = 'SYNO.CloudSync'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {
+            'version': info['minVersion'], 
+            'method': 'get_selective_folder_list',
+            'session_id': sess_id,
+            'path': path,
+            'file_id': remote_folder_id,
+            'exists_type': 'null'
+        }
+
+        return self.request_data(api_name, api_path, req_param)
