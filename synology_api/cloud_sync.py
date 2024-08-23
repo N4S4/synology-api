8fcf3ee660742092f33869f2a85bf3b241530c54
joeperpetua <qwerty.perpetua@gmail.com>
2024-08-23 10:35:32 +0200

    Adding class description

--- synology_api/cloud_sync.py
+++ synology_api/cloud_sync.py
@@ -5,6 +5,16 @@ from . import base_api
 class CloudSync(base_api.BaseApi):
     '''
        Cloud Sync API implementation.
+
+       This API provides the functionality to get information related to the package settings and current connections and tasks. 
+       It also provides functionalities to set most of the settings for tasks and package configuration, as well as manage the current syncing processes.
+
+       Due to the vast amount of public clouds available in the project, the API was not tested for every cloud scenario, so some params request may be missing in specific not tested clouds.
+
+       The tested clouds so far are:
+       - Google Drive  
+       - OneDrive
+       - DropBox
     '''
 
     def get_config(self) -> dict[str, object] | str:
