826c7411f9826e668f227d037525d55d53fcae67
joeperpetua <qwerty.perpetua@gmail.com>
2024-08-23 10:54:02 +0200

    Adding getters for connections

--- synology_api/cloud_sync.py
+++ synology_api/cloud_sync.py
@@ -19,7 +19,7 @@ class CloudSync(base_api.BaseApi):
 
     def get_config(self) -> dict[str, object] | str:
         '''
-        Get package settings.
+        Return package settings.
         '''
         api_name = 'SYNO.CloudSync'
         info = self.gen_list[api_name]
@@ -31,4 +31,72 @@ class CloudSync(base_api.BaseApi):
 
         return self.request_data(api_name, api_path, req_param)
     
+    def get_connections(self, group_by: str = 'group_by_user') -> dict[str, object] | str:
+        '''
+        Return list of current cloud connections.
+
+        group_by = 'group_by_user' ||  'group_by_cloud_type'
+        '''
+        api_name = 'SYNO.CloudSync'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {
+            'version': info['minVersion'], 
+            'method': 'list_conn',
+            'is_tray': False,
+            'group_by': group_by
+        }
+
+        return self.request_data(api_name, api_path, req_param)
+    
+    def get_connection_settings(self, conn_id: int) -> dict[str, object] | str:
+        '''
+        Return settings from a given connection.
+
+        conn_id = int from get_connections
+        '''
+        api_name = 'SYNO.CloudSync'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {
+            'version': info['minVersion'], 
+            'method': 'get_connection_setting',
+            'connection_id': conn_id
+        }
+
+        return self.request_data(api_name, api_path, req_param)
+    
+    def get_connection_information(self, conn_id: int) -> dict[str, object] | str:
+        '''
+        Return information from a given connection.
+
+        conn_id = int from get_connections
+        '''
+        api_name = 'SYNO.CloudSync'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {
+            'version': info['minVersion'], 
+            'method': 'get_property',
+            'connection_id': conn_id
+        }
+
+        return self.request_data(api_name, api_path, req_param)
+    
+    def get_connection_auth(self, conn_id: int) -> dict[str, object] | str:
+        '''
+        Return authentication information from a given connection.
+
+        conn_id = int from get_connections
+        '''
+        api_name = 'SYNO.CloudSync'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {
+            'version': info['minVersion'], 
+            'method': 'get_conn_auth_info',
+            'connection_id': conn_id
+        }
+
+        return self.request_data(api_name, api_path, req_param)
 
