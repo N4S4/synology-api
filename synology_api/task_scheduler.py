6587ca78c1864e4d6b90aebd6fc18a0d62910d10
joeperpetua <qwerty.perpetua@gmail.com>
2024-09-11 13:33:15 +0200

    Add script task create method and Schedule interface

--- synology_api/task_scheduler.py
+++ synology_api/task_scheduler.py
@@ -2,6 +2,70 @@ from __future__ import annotations
 from . import base_api
 import json
 
+class _Schedule():
+    def __init__(
+            self,
+            run_frequently: bool = True, # date_type
+            run_days: str = '0,1,2,3,4,5,6', # week_days
+            run_date: str = '', # date
+            repeat: str = 'Daily',
+            monthly_week = [],
+            start_time_h: int = 0,
+            start_time_m: int = 0,
+            same_day_repeat_h: int = 0,
+            same_day_repeat_m: int = 0,
+            same_day_repeat_until: int = 0,
+        ):
+        self.run_frequently = run_frequently
+        self.run_days = run_days
+        self.run_date = run_date
+        self.repeat = repeat
+        self.monthly_week = monthly_week
+        self.start_time_h = start_time_h
+        self.start_time_m = start_time_m
+        self.same_day_repeat_h = same_day_repeat_h
+        self.same_day_repeat_m = same_day_repeat_m
+        self.same_day_repeat_until = same_day_repeat_until
+
+    def _generate_dict(self) -> dict:
+        schedule_dict = {
+            'date_type': self.run_frequently,
+            'monthly_week': json.dumps(self.monthly_week),
+            'hour': self.start_time_h,                    # Start time - Hour for the schedule
+            'minute': self.start_time_m,                  # Start time - Minute for the schedule
+            'repeat_hour': self.same_day_repeat_h,        # Continue running on the same day - Repeat each X hours 1..23 // 0 = disabled
+            'repeat_min': self.same_day_repeat_m,         # Continue running on the same day - Repeat every X minute [1, 5, 10, 15, 20, 30] // 0 = disabled
+            'last_work_hour': self.same_day_repeat_until, # Last run time
+        }
+        repeat_modality = -1
+
+        if self.run_frequently:
+            if self.repeat == 'Daily':
+                repeat_modality = 1001 
+            if self.repeat == 'Weekly':
+                repeat_modality = 1002
+            if self.repeat == 'Monthly':
+                repeat_modality = 1003
+            
+            schedule_dict['week_day'] = self.run_days
+        
+        if self.run_frequently == 1:
+            if self.repeat == 'No repeat':
+                repeat_modality = 0 
+            if self.repeat == 'Monthly':
+                repeat_modality = 1
+            if self.repeat == 'Every 3 months':
+                repeat_modality = 5
+            if self.repeat == 'Every 6 months':
+                repeat_modality = 3
+            if self.repeat == 'Yearly':
+                repeat_modality = 2
+            
+            schedule_dict['date'] = self.run_date
+
+        schedule_dict['repeat_date'] = repeat_modality
+
+        return schedule_dict
 
 class TaskScheduler(base_api.BaseApi):
     """
@@ -23,7 +87,7 @@ class TaskScheduler(base_api.BaseApi):
 
        To implement in the future:
        - Add retention settings for Recycle bin task set/create methods.
-    """
+    """  
 
     def get_output_config(self) -> dict[str, object] | str:
         """
@@ -294,3 +358,53 @@ class TaskScheduler(base_api.BaseApi):
         }
 
         return self.request_data(api_name, api_path, req_param)
+    
+    def create_script_task(
+            self,
+            task_name: str,
+            owner: str,
+            script: str,
+            enable: bool = True,
+            run_frequently: bool = True, # date_type
+            run_days: str = '0,1,2,3,4,5,6', # week_days
+            run_date: str = '', # date
+            repeat: str = 'Daily',
+            monthly_week = [],
+            start_time_h: int = 0,
+            start_time_m: int = 0,
+            same_day_repeat_h: int = 0,
+            same_day_repeat_m: int = 0,
+            same_day_repeat_until: int = 0,
+            notify_email: str = '',
+            notify_error: bool = False
+        ) -> dict[str, object] | str:
+
+        schedule = _Schedule(run_frequently, run_days, run_date, repeat, monthly_week, start_time_h, start_time_m,
+                            same_day_repeat_h, same_day_repeat_m, same_day_repeat_until)
+        
+        schedule_dict = schedule._generate_dict()
+        print(schedule_dict)
+
+        extra = {
+            'notify_enable': 'true' if notify_email is not '' else 'false',
+            'notify_mail': notify_email,
+            'notify_if_error': notify_error,
+            'script': script
+        }
+
+        api_name = 'SYNO.Core.TaskScheduler'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {
+            'version': 4, 
+            'method': 'create',
+            'name': task_name,
+            'real_owner': owner,
+            'owner': owner,
+            'enable': enable,
+            'schedule': json.dumps(schedule_dict),
+            'extra': json.dumps(extra)
+        }
+
+        return self.request_data(api_name, api_path, req_param)
+
