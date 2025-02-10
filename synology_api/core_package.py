cc55de9321854c7287ea3d074c2ac28171c52b3f
fboissadier <florent.boissadier@wanadoo.fr>
2025-02-10 23:41:43 +0100

    update docstring

--- synology_api/core_package.py
+++ synology_api/core_package.py
@@ -13,42 +13,42 @@ class Package(base_api.BaseApi):
         """List installed packages
             Parameters
             ----------
-                additional : list[str], optional
-                        Additional fields to retrieve. Defaults to `[]`.
-                        All fields known are: 
-                            `["description", "description_enu", "dependent_packages", "beta", "distributor", "distributor_url",
-                            "maintainer", "maintainer_url", "dsm_apps", "dsm_app_page", "dsm_app_launch_name","report_beta_url",
-                            "support_center", "startable", "installed_info", "support_url", "is_uninstall_pages","install_type",
-                            "autoupdate", "silent_upgrade", "installing_progress", "ctl_uninstall", "updated_at", "status",
-                            "url","available_operation"]`.
-                ignore_hidden : bool
-                    TODO: Write description
+            additional : list[str], optional
+                    Additional fields to retrieve. Defaults to `[]`.
+                    All fields known are: 
+                        `["description", "description_enu", "dependent_packages", "beta", "distributor", "distributor_url",
+                        "maintainer", "maintainer_url", "dsm_apps", "dsm_app_page", "dsm_app_launch_name","report_beta_url",
+                        "support_center", "startable", "installed_info", "support_url", "is_uninstall_pages","install_type",
+                        "autoupdate", "silent_upgrade", "installing_progress", "ctl_uninstall", "updated_at", "status",
+                        "url","available_operation"]`.
+            ignore_hidden : bool
+                TODO: Write description
                     
             Returns
             -------
-                dict
-                    List of packages installed on the NAS
+            dict
+                List of packages installed on the NAS
 
             Example return
             --------------
-                ```json
-                {
-                    "data": {
-                        "packages": [
-                            {
-                                "additional": {
-                                    "install_type": ""
-                                },
-                                "id": "ActiveBackup-Office365",
-                                "name": "Active Backup for Microsoft 365",
-                                "timestamp": 1738880043640,
-                                "version": "2.5.5-14034"
-                            }
-                        ]
-                    },
-                    "success": true
-                }
-                ```
+            ```json
+            {
+                "data": {
+                    "packages": [
+                        {
+                            "additional": {
+                                "install_type": ""
+                            },
+                            "id": "ActiveBackup-Office365",
+                            "name": "Active Backup for Microsoft 365",
+                            "timestamp": 1738880043640,
+                            "version": "2.5.5-14034"
+                        }
+                    ]
+                },
+                "success": true
+            }
+            ```
         """
         api_name = 'SYNO.Core.Package'
         info = self.core_list[api_name]
@@ -64,25 +64,24 @@ class Package(base_api.BaseApi):
     
     def list_installable(self) -> dict:
         """List installable packages
-
             Returns
             -------
-                dict
-                    List of beta_package, categories and packages available
+            dict
+                List of beta_package, categories and packages available
                     
             Example return
             --------------
-                ```json
-                {
-                    "data": {
-                        "banners": [],
-                        "beta_packages": [...],
-                        "categories": [...],
-                        "packages": [...]
-                    },
-                    "success": true
-                }
-                ```
+            ```json
+            {
+                "data": {
+                    "banners": [],
+                    "beta_packages": [...],
+                    "categories": [...],
+                    "packages": [...]
+                },
+                "success": true
+            }
+            ```
         """
         api_name = 'SYNO.Core.Package.Server'
         info = self.core_list[api_name]
@@ -98,51 +97,50 @@ class Package(base_api.BaseApi):
     
     def get_package_center_settings(self) -> dict:
         """Get package center settings
-
             Returns
             -------
-                dict
-                    List settings of the Package center
-                    
+            dict
+                List settings of the Package center
+                
             Example return
             --------------
-                ```json
-                {
-                    "data": {
-                        "autoupdateall": false,
-                        "autoupdateimportant": true,
-                        "default_vol": "/volume1",
-                        "enable_autoupdate": true,
-                        "enable_dsm": true,
-                        "enable_email": false,
-                        "mailset": true,
-                        "trust_level": 0,
-                        "update_channel": true,
-                        "volume_count": 2,
-                        "volume_list": [
-                            {
-                                "desc": "",
-                                "display": "Volume 1 (Available capacity:  184.72 GB )",
-                                "mount_point": "/volume1",
-                                "size_free": "198336327680",
-                                "size_total": "206158430208",
-                                "vol_desc": "Apps",
-                                "volume_features": []
-                            },
-                            {
-                                "desc": "",
-                                "display": "Volume 2 (Available capacity:  2391.14 GB )",
-                                "mount_point": "/volume2",
-                                "size_free": "2567467421696",
-                                "size_total": "3623234412544",
-                                "vol_desc": "Stockage",
-                                "volume_features": []
-                            }
-                        ]
-                    },
-                    "success": true
-                }
-                ```
+            ```json
+            {
+                "data": {
+                    "autoupdateall": false,
+                    "autoupdateimportant": true,
+                    "default_vol": "/volume1",
+                    "enable_autoupdate": true,
+                    "enable_dsm": true,
+                    "enable_email": false,
+                    "mailset": true,
+                    "trust_level": 0,
+                    "update_channel": true,
+                    "volume_count": 2,
+                    "volume_list": [
+                        {
+                            "desc": "",
+                            "display": "Volume 1 (Available capacity:  184.72 GB )",
+                            "mount_point": "/volume1",
+                            "size_free": "198336327680",
+                            "size_total": "206158430208",
+                            "vol_desc": "Apps",
+                            "volume_features": []
+                        },
+                        {
+                            "desc": "",
+                            "display": "Volume 2 (Available capacity:  2391.14 GB )",
+                            "mount_point": "/volume2",
+                            "size_free": "2567467421696",
+                            "size_total": "3623234412544",
+                            "vol_desc": "Stockage",
+                            "volume_features": []
+                        }
+                    ]
+                },
+                "success": true
+            }
+            ```
         """
         api_name = 'SYNO.Core.Package.Setting'
         info = self.core_list[api_name]
@@ -160,9 +158,8 @@ class Package(base_api.BaseApi):
                         default_vol: str, update_channel: str
             ) -> dict:
         """Set settings of the package center
-
-        Parameters
-        ----------
+            Parameters
+            ----------
             enable_email : bool
                 Enable email notification
                 
@@ -185,14 +182,13 @@ class Package(base_api.BaseApi):
                 "stable" => Disable beta packages
                 "beta" => Enable beta packages
 
-
-        Returns
-        -------
+            Returns
+            -------
             dict
                 Return some settings
 
-        Example return
-        --------------
+            Example return
+            --------------
             ```json
             {
                 "data": {
@@ -224,45 +220,44 @@ class Package(base_api.BaseApi):
     
     def get_package_center_infos(self) -> dict:
         """Get package center informations
-
             Returns
             -------
-                dict
-                    List of configs
+            dict
+                List of configs
         
             Return example
             --------------
-                ```json
-                {
-                    "data": {
-                        "config": {
-                            "auth_key": "------------------------------",
-                            "blBetaChannel": false,
-                            "blOtherServer": false,
-                            "def_void": "",
-                            "ds_build": "72806",
-                            "ds_major": "7",
-                            "ds_minor": "2",
-                            "ds_timezone": "Amsterdam",
-                            "ds_unique": "synology_r1000_723+",
-                            "myPayBaseURL": "https://payment.synology.com",
-                            "myds_id": "7886858",
-                            "serial": "2260TPR7X30E6",
-                            "success": true
-                        },
-                        "prerelease": {
-                            "agreed": true,
-                            "success": true
-                        },
-                        "term": {
-                            "agreed_term_version": "0003",
-                            "curr_term_version": "0003",
-                            "success": true
-                        }
+            ```json
+            {
+                "data": {
+                    "config": {
+                        "auth_key": "------------------------------",
+                        "blBetaChannel": false,
+                        "blOtherServer": false,
+                        "def_void": "",
+                        "ds_build": "72806",
+                        "ds_major": "7",
+                        "ds_minor": "2",
+                        "ds_timezone": "Amsterdam",
+                        "ds_unique": "synology_r1000_723+",
+                        "myPayBaseURL": "https://payment.synology.com",
+                        "myds_id": "7886858",
+                        "serial": "2260TPR7X30E6",
+                        "success": true
                     },
-                    "success": true
-                }
-                ```
+                    "prerelease": {
+                        "agreed": true,
+                        "success": true
+                    },
+                    "term": {
+                        "agreed_term_version": "0003",
+                        "curr_term_version": "0003",
+                        "success": true
+                    }
+                },
+                "success": true
+            }
+            ```
         """
         api_name = 'SYNO.Core.Package.Info'
         info = self.core_list[api_name]
@@ -275,7 +270,28 @@ class Package(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def check_install(self, packages: List[str]) -> dict:
-        """TODO: Docstring
+        """Check if installation is possible
+        
+            Parameters
+            ----------
+            packages : List[str]
+                _description_
+        
+            Returns
+            -------
+            dict
+                _description_
+        
+            Example return
+            ----------
+            ```json
+            {
+                "data": {
+                    "template": "data"
+                },
+                "success": true
+            }
+            ```
         """
         api_name = 'SYNO.Core.Package.Setting'
         info = self.core_list[api_name]
