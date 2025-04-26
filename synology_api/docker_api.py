a6b5465881b2fbfda9e5f357834ebab29d91a2fb
Renato <33936751+N4S4@users.noreply.github.com>
2025-04-26 17:34:14 +0200

    added functions to docker_api

--- synology_api/docker_api.py
+++ synology_api/docker_api.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from typing import Optional
+
+import synology_api.auth
 from . import base_api
 
 
@@ -51,5 +53,81 @@ class Docker(base_api.BaseApi):
         info = self.gen_list[api_name]
         api_path = info['path']
         req_param = {'version': info['maxVersion'], 'method': 'list'}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    #TODO not working
+    def search_image(self, query : str = None) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Registry'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'search', 'offset': 0, 'limit': 50, 'page_size': 50, 'q': query}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    def list_projects(self) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Project'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'list'}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    #TODO not working with project_id
+    def get_project_info(self, project_id : str = None) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Project'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'get', 'name': project_id}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    def start_container(self, container : str = None) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Container'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'start', 'name': container}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    def stop_container(self, container : str = None) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Container'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'stop', 'name': container}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    def export_container_settings(self, container : str = None, path : str = None) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Container.Profile'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'export', 'name': container, 'path' : path}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    def export_container(self, container : str = None, path : str = None) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Container'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'export', 'name': container, 'path' : path}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    #TODO not working
+    def get_logs(self) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Log'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'list'}#, 'action': '"load"', 'offset': 0, 'limit': 1000,
+                     #'sort_by': 'time', 'sort_dir': 'DESC', 'datefrom': 0, 'dateto': 0}
+
+        return self.request_data(api_name, api_path, req_param)
+
+    def docker_stats(self) -> dict[str, object] | str:
+        api_name = 'SYNO.Docker.Container'
+        info = self.gen_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'stats'}
 
         return self.request_data(api_name, api_path, req_param)
