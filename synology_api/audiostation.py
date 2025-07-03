74ad8d8e4639a85f52a35bd0abf39e34ee644d46
Lubos Zima <luboszima@gmail.com>
2025-07-03 20:59:35 +0700

    fix: synology_api/audiostation.py update docstrings to comply with Numpydoc standards

--- synology_api/audiostation.py
+++ synology_api/audiostation.py
@@ -1,11 +1,21 @@
+"""AudioStation API module for interacting with Synology AudioStation services."""
 from __future__ import annotations
 from typing import Optional
 from . import base_api
 
 
 class AudioStation(base_api.BaseApi):
+    """A class to interact with Synology AudioStation API."""
 
     def get_info(self) -> dict[str, object] | str:
+        """
+        Retrieve general information about the AudioStation service.
+
+        Returns
+        -------
+        dict[str, object]
+            A dictionary containing the service information or a string in case of an error.
+        """
         api_name = 'SYNO.AudioStation.Info'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -13,6 +23,14 @@ class AudioStation(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def get_playlist_info(self) -> dict[str, object] | str:
+        """
+        Retrieve information about playlists in AudioStation.
+
+        Returns
+        -------
+        dict[str, object]
+            A dictionary containing playlist information or a string in case of an error.
+        """
         api_name = 'SYNO.AudioStation.Playlist'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -22,6 +40,14 @@ class AudioStation(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def list_remote_player(self) -> dict[str, object] | str:
+        """
+        Retrieve a list of remote players available in AudioStation.
+
+        Returns
+        -------
+        dict[str, object]
+            A dictionary containing information about remote players, or a string in case of an error.
+        """
         api_name = 'SYNO.AudioStation.RemotePlayer'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -31,6 +57,14 @@ class AudioStation(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def list_pinned_song(self) -> dict[str, object] | str:
+        """
+        Retrieve a list of pinned songs in AudioStation.
+
+        Returns
+        -------
+        dict[str, object]
+            A dictionary containing information about pinned songs, or a string in case of an error.
+        """
         api_name = 'SYNO.AudioStation.Pin'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -39,6 +73,19 @@ class AudioStation(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def device_id(self, device: str) -> dict[str, object] | str:
+        """
+        Retrieve the playlist for a specific remote device in AudioStation.
+
+        Parameters
+        ----------
+        device : str
+            The ID of the remote device.
+
+        Returns
+        -------
+        dict[str, object]
+            A dictionary containing the playlist information for the specified device.
+        """
         api_name = 'SYNO.AudioStation.RemotePlayer'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -50,6 +97,19 @@ class AudioStation(base_api.BaseApi):
     # You Must choose the device if any from list_remote_player()
 
     def remote_play(self, device: str) -> dict[str, object] | str:
+        """
+        Start playback on a specified remote device in AudioStation.
+
+        Parameters
+        ----------
+        device : str
+            The ID of the remote device on which to start playback.
+
+        Returns
+        -------
+        dict[str, object] or str
+            A dictionary containing the playback status or a string in case of an error.
+        """
         api_name = 'SYNO.AudioStation.RemotePlayer'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -59,6 +119,19 @@ class AudioStation(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def remote_stop(self, device: str) -> dict[str, object] | str:
+        """
+        Stop playback on a specified remote device in AudioStation.
+
+        Parameters
+        ----------
+        device : str
+            The ID of the remote device on which to stop playback.
+
+        Returns
+        -------
+        dict[str, object] or str
+            A dictionary containing the stop status or a string in case of an error.
+        """
         api_name = 'SYNO.AudioStation.RemotePlayer'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -68,6 +141,19 @@ class AudioStation(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def remote_next(self, device: str) -> dict[str, object] | str:
+        """
+        Skip to the next track on a specified remote device in AudioStation.
+
+        Parameters
+        ----------
+        device : str
+            The ID of the remote device on which to skip to the next track.
+
+        Returns
+        -------
+        dict[str, object] or str
+            A dictionary containing the status of the operation or a string in case of an error.
+        """
         api_name = 'SYNO.AudioStation.RemotePlayer'
         info = self.gen_list[api_name]
         api_path = info['path']
@@ -77,6 +163,19 @@ class AudioStation(base_api.BaseApi):
         return self.request_data(api_name, api_path, req_param)
 
     def remote_prev(self, device: str) -> dict[str, object] | str:
+        """
+        Skip to the previous track on a specified remote device in AudioStation.
+
+        Parameters
+        ----------
+        device : str
+            The ID of the remote device on which to skip to the previous track.
+
+        Returns
+        -------
+        dict[str, object] or str
+            A dictionary containing the status of the operation or a string in case of an error.
+        """
         api_name = 'SYNO.AudioStation.RemotePlayer'
         info = self.gen_list[api_name]
         api_path = info['path']
